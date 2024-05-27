1a17149b6eeaeb7145c8fe53989d3603c47b049e
ajitlenka30 <ajitlenka30@gmail.com>
2024-05-27 18:16:39 +0530

    sweet: Build health hal 2.1-impl Signed-off-by: ajitlenka30 <ajitlenka30@gmail.com>

--- device.mk
+++ device.mk
@@ -254,9 +254,9 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1.vendor \
-    android.hardware.health-service.qti \
-    android.hardware.health-service.qti_recovery
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service \
+    android.hardware.health@2.1-impl.recovery
 
 # Thermal
 ifeq ($(TARGET_USE_QTI_THERMAL_SERVICE),true)
